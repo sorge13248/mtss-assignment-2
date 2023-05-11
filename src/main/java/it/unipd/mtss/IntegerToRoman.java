33e364144ec9bdf4622099095fa3ac2154eb3e16
Francesco Sorge <4579797+sorge13248@users.noreply.github.com>
2023-05-11 14:01:01 +0200

    Implementata conversione numeri closes #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,8 +1,26 @@
+////////////////////////////////////////////////////////////////////
+// Francesco Sorge 1170715
+// Enrico Zangrando 2000547
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+
     public static String convert(int number) {
-        // TODO
-        return null;
+        String[] romanNums = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        int[] arabicNums = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        StringBuilder romanNumStringBuilder = new StringBuilder();
+        int i = 0;
+
+        while (number > 0) {
+            while (number >= arabicNums[i]) {
+                romanNumStringBuilder.append(romanNums[i]);
+                number -= arabicNums[i];
+            }
+            i++;
+        }
+
+        return romanNumStringBuilder.toString();
     }
 }
